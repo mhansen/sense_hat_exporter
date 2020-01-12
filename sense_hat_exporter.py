abaf92c463034af053e08b4a581a5eca4aa3b85f
Mark Hansen <mark@markhansen.co.nz>
2020-01-12 14:22:51 +1100

    Add both temperature sensors and accelerometer

--- sense_hat_exporter.py
+++ sense_hat_exporter.py
@@ -7,10 +7,7 @@ sense = SenseHat()
 
 class SenseHatCollector(object):
     def collect(self):
-        yield GaugeMetricFamily(
-            name='sense_hat_temperature_celsius',
-            documentation='Temperature as measured by Raspberry Pi Sense Hat',
-            value=sense.temperature)
+        yield temperature_metric()
         yield GaugeMetricFamily(
             name='sense_hat_pressure_pascals',
             documentation='Pressure (Pa) Measured by Raspberry Pi Sense Hat',
@@ -20,12 +17,34 @@ class SenseHatCollector(object):
             documentation='Measured by Raspberry Pi Sense Hat',
             value=sense.humidity)
         yield compass_metric()
+        yield accelerometer_metric()
+
+def temperature_metric():
+    family = GaugeMetricFamily(
+        name='sense_hat_temperature_celsius',
+        documentation='Temperature as measured by Raspberry Pi Sense Hat',
+        labels = ["source_sensor"])
+    family.add_metric(["humidity"], sense.get_temperature_from_humidity())
+    family.add_metric(["pressure"], sense.get_temperature_from_pressure())
+    return family
 
 def compass_metric():
     raw = sense.get_compass_raw()
 
     family = GaugeMetricFamily(
             name='sense_hat_compass_raw',
+            documentation='Measured by Raspberry Pi Sense Hat',
+            labels=["axis"])
+    family.add_metric(["x"], raw["x"])
+    family.add_metric(["y"], raw["y"])
+    family.add_metric(["z"], raw["z"])
+    return family
+
+def accelerometer_metric():
+    raw = sense.get_accelerometer_raw()
+
+    family = GaugeMetricFamily(
+            name='sense_hat_accelerometer_raw',
             documentation='Measured by Raspberry Pi Sense Hat',
             labels=["axis"])
     family.add_metric(["x"], raw["x"])
